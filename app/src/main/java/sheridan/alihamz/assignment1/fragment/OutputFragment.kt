12c97aebb55a146969b80161a402cc182a54847c
aliy9914 <87914112+aliy9914@users.noreply.github.com>
2021-10-08 16:28:46 -0400

    Output Fragment

--- app/src/main/java/sheridan/alihamz/assignment1/fragment/OutputFragment.kt
+++ app/src/main/java/sheridan/alihamz/assignment1/fragment/OutputFragment.kt
@@ -1,62 +1,115 @@
 package sheridan.alihamz.assignment1.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import sheridan.alihamz.assignment1.R
-import android.widget.Toast
 import sheridan.alihamz.assignment1.Util.Utils
+import sheridan.alihamz.assignment1.databinding.FragmentOutputBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [OutputFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class OutputFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    private var _binding: FragmentOutputBinding? = null
+    private val binding get() = _binding!!
+
+
+    private var userInput: Int? = 0
+    private var isUserWin: Boolean? = false
+
+
+    companion object {
+        fun newInstance(userInput: Int): Fragment {
+            val fragment = OutputFragment()
+            val args = Bundle()
+            args.putInt("userInput", userInput)
+            fragment.arguments = args
+            return fragment
         }
     }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.output_fragment, container, false)
+
+        _binding = FragmentOutputBinding.inflate(inflater, container, false)
+        val view = binding.root
+        userInput = arguments?.getInt("userInput", 0)
+
+        binding.btnPlayAgain.setOnClickListener(View.OnClickListener {
+            requireActivity().onBackPressed()
+        })
+        init()
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment OutputFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            OutputFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    private fun init() {
+        binding.tvUser.text = getSelection(userInput);
+        /**get random selection from computer*/
+        val computer_input = (1..3).random()
+        binding.tvComputer.text = getSelection(computer_input)
+
+        if (userInput == computer_input) {
+            /** for same input match is DRAW*/
+            binding.tvWinner.text = resources.getString(R.string.lbl_draw)
+            binding.tvWinner.setTextColor(ContextCompat.getColor(requireActivity(), R.color.grey_dark))
+
+        } else {
+            Log.e("WW","$userInput : $computer_input")
+            if (userInput == Utils.PAPER) {
+                /**Paper wrap Rock*/
+                if (computer_input == Utils.ROCK) {
+                    isUserWin = true;
+                } else if (computer_input == Utils.SCISSORS) {
+                    isUserWin = false;
+                }
+            } else if (userInput == Utils.ROCK) {
+                /**Rock blunts Scissors*/
+                if (computer_input == Utils.SCISSORS) {
+                    isUserWin = true;
+                } else if (computer_input == Utils.PAPER) {
+                    isUserWin = false;
+                }
+            } else if (userInput == Utils.SCISSORS) {
+                /**Scissors cuts paper */
+                if (computer_input == Utils.PAPER) {
+                    isUserWin = true;
+                } else if (computer_input == Utils.ROCK) {
+                    isUserWin = false;
                 }
             }
+
+            if (isUserWin!!) {
+                binding.tvWinner.text = resources.getString(R.string.lbl_user)
+                binding.tvWinner.setTextColor(ContextCompat.getColor(requireActivity(), R.color.green))
+
+            } else {
+                binding.tvWinner.text = resources.getString(R.string.lbl_computer)
+                binding.tvWinner.setTextColor(ContextCompat.getColor(requireActivity(), R.color.red))
+
+
+            }
+        }
     }
+
+    private fun getSelection(inputValue: Int?): CharSequence? {
+        /** Fetch the string value from its index position
+         * for display in screen */
+
+        if (inputValue == Utils.PAPER) {
+            return resources.getString(R.string.lbl_paper)
+        } else if (inputValue == Utils.ROCK) {
+            return resources.getString(R.string.lbl_rock)
+        } else {
+            return resources.getString(R.string.lbl_scissors)
+        }
+    }
+
+
 }
